02fe3ae926eb75e6d3b3d933a7759f8a4d21a2d5
mmmppp <maksim78998@gmail.com>
2022-03-11 16:04:25 +0300

    try to зашифровать

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import math
-from Crypto.Util.number import *
+from binascii import *
+
 
 def prime(N):
-    if N <2:
+    if N < 2:
         return 0
-    for i in range (2, int(math.sqrt(N))):
+    for i in range(2, int(math.sqrt(N))):
         if N % i == 0:
             return 1
     return 0
 
 
 def m_prime(A, B):
-    while(A and B):
+    while (A and B):
         if (A > B):
             A = A % B
         else:
@@ -22,47 +23,68 @@ def m_prime(A, B):
         return 0
 
 
-def mod_inv(a , n):
+def mod_inv(a, n):
     k = 1
-    while(1):
+    while (1):
         if (k * n + 1) % a == 0:
-            return (k * n + 1)//a
+            return (k * n + 1) // a
         else:
             k += 1
 
 
-print(long_to_bytes(350251725201363341802583308674393245081890473196190184232598667822223711226275068721872255925929137864882558012681478820032946410619079932194838177403168344897812999450289328454857332))
 print("Hello!!!")
 
 
-p = int(input("p="))
-while(prime(p) != 0):
+
+p = 337 #int(input("p="))
+while (prime(p) != 0):
     p = int(input("please,try again\n"))
 
-
-q = int(input("q="))
-while(prime(q) != 0):
+q = 997 #int(input("q="))
+while (prime(q) != 0):
     q = int(input("please,try again\n"))
-
 
 n = p * q
 phi = (p - 1) * (q - 1)
 
+print("e[1;", phi, "]:")
+e = 5#int(input())
+while ((e <= 1) or (e >= phi) or (m_prime(e, phi) != 1)):
+    e = int(input("please,try again\n"))
 
-if (phi > 0):
-    print("e[1;",phi,"]:")
-    e = int(input())
-    while((e <= 1) or (e >= phi) or (m_prime(e , phi) != 1)):
-        e = int(input("please,try again\n"))
 
-c = int(input("c ="))
+
+#______________________________________
+
+m = str(input("text your message:"))
+
+m = m.encode("utf-8").hex()
+
+print(m)
+
+m = int(m,16)
+
+print(m)
+
+c = pow(m, e, n)
+print("c=",c)
+# ______________________________________________
+# decryption
+
 
 # pow(-1, e, phi)
-d = mod_inv( e, phi )
-print ( "d =", d)
+d = mod_inv(e, phi)
+print("d =", d)
 
-# m = pow(c, d)%n
-m =pow(c , d, n)
-# plaintext = str(long_to_bytes(m,2))[3:-1]
-plaintext = long_to_bytes(m,2).decode()
-print(plaintext)
+m = pow(c, d)
+m = m % n
+print("m10=",m)
+m = hex(m)
+
+print("m16=",m)
+
+m = '0'+ m[2:]
+print(m)
+
+print(unhexlify(m).decode())
+
